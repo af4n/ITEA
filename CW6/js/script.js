6f41763c43d07dd1dd5785e6a83779253dbd116e
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-03-16 01:07:01 +0200

    add hw7

--- CW6/js/script.js
+++ CW6/js/script.js
@@ -1,22 +1,31 @@
 window.addEventListener("DOMContentLoaded", (e) => {
 
-    const 
+    const sousA = document.getElementsByClassName('sous'),
+        toppingA = document.getElementsByClassName('topping'),
+        sizeA = document.getElementsByClassName('size');
+        
+    for (let i=0; i<sousA.length; i++) {
+        const sous = sousA[i];
+        sous.addEventListener('click', function (e) {
+            console.log(sous.dataset.info);
+            document.querySelector('.price > h2').innerHTML += sous.dataset.info;
+            
+        });
+    };
 
-    const sousKlassicheskij = {
-        name: 'Klassicheskij',
-        price: 10
-    },
-    sousBbq = {
-        name: 'bbq',
-        price: 10
-    },
-    sousRikotta = {
-        name: 'rikotta',
-        price: 10
-    }
+    for (let i=0; i<toppingA.length; i++) {
+        const topping = toppingA[i];
+        topping.addEventListener('click', function (e) {
+            console.log(topping.dataset.info);
+            document.querySelector('.price > h2').innerHTML += topping.dataset.info;
+        })
+    };
 
-    for(let div of document.querySelectorAll('[show-info]')) {
-        let field = div.getAttribute('show-info');
-        div.innerHTML = 
-
-})
+    for (let i=0; i<sizeA.length; i++) {
+        const size = sizeA[i];
+        size.addEventListener('click', function (e) {
+            console.log(size.dataset.info);
+            document.querySelector('.price > h2').innerHTML += size.dataset.info;
+        })
+    };
+});
